0804874e42945f7682817eb1d052eea4fa469d83
Nick <nhorton04@gmail.com>
2018-12-01 12:50:49 -0800

    setup environment

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const { GraphQLServer } = require('graphql-yoga')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test');
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+  },
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+mongoose.connection.once('open', function() {
+  server.start(() => console.log('Server is running on localhost:4000'))
+});
