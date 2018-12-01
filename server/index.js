19e8ca3a862d3f755fa40d20f86c7f9434de77a1
Nick <nhorton04@gmail.com>
2018-12-01 13:22:31 -0800

    crud operations functional

--- server/index.js
+++ server/index.js
@@ -3,17 +3,49 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/test');
 
+const Todo = mongoose.model('Todo',  {
+  text: String,
+  complete: Boolean
+});
+
 const typeDefs = `
   type Query {
     hello(name: String): String!
+    todos: [Todo]
   }
-`
+  type Todo {
+    id: ID!
+    text: String!
+    complete: Boolean!
+  }
+  type Mutation {
+    createTodo(text: String!): Todo
+    updateTodo(id: ID!, complete: Boolean!): Boolean
+    removeTodo(id: ID!): Boolean
+  }
+`;
 
 const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || 'World'}`,
+    todos: () => Todo.find()
   },
-}
+  Mutation: {
+    createTodo: async (_, { text}) => {
+      const todo = new Todo({ text, complete: false });
+      await todo.save();
+      return todo;
+    },
+    updateTodo: async (_, { id, complete }) => {
+      await Todo.findByIdAndUpdate(id, { complete });
+      return true;
+    },
+    removeTodo: async (_, { id }) => {
+      await Todo.findByIdAndRemove(id);
+      return true;
+    }
+  }
+};
 
 const server = new GraphQLServer({ typeDefs, resolvers })
 mongoose.connection.once('open', function() {
